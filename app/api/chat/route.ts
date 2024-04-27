23cac6942eb92414db88596ca50aae73d5e5f49f
Bhogeshwar Jadhav <bhogeshwarj@gmail.com>
2024-04-27 10:56:46 +0530

    changed how to invoke gemini-model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,40 +1,40 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
  
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
- 
+
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
- 
+
 // convert messages from the Vercel AI SDK Format to the format
 // that is expected by the Google GenAI SDK
 const buildGoogleGenAIPrompt = (messages: Message[]) => ({
   contents: messages
-    .filter(message => message.role === 'user' || message.role === 'assistant')
-    .map(message => ({
-      role: message.role === 'user' ? 'user' : 'model',
-      parts: [{ text: message.content }],
-    })),
+  .filter(message => message.role === 'user' || message.role === 'assistant')
+  .map(message => ({
+    role: message.role === 'user' ? 'user' : 'model',
+    parts: [{ text: message.content }],
+  })),
 });
 
-   // Get the API key from the query parameter
-  //  const {searchParams} = new URL(req.url);
-  //  const apiKey = searchParams.get('key');
+// Get the API key from the query parameter
+//  const {searchParams} = new URL(req.url);
+//  const apiKey = searchParams.get('key');
 
-  //  // Validate the API key
-  //  if (!apiKey) {
+//  // Validate the API key
+//  if (!apiKey) {
   //    return res.status(401).json({ message: 'Invalid API key' });
   //  }
   export async function POST(req: Request,res:Response) {
     try {
+      const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
       // const apiKey = process.env.GOOGLE_API_KEY;
-    const { messages } = await req.json();
-    
-    console.log("Received messages:", messages);
-    //  const genAI = new GoogleGenerativeAI(apiKey);
+      const { messages } = await req.json();
+      
+      console.log("Received messages:", messages);
+      //  const genAI = new GoogleGenerativeAI(apiKey);
 
     const geminiStream = await genAI
-    .getGenerativeModel({ model: 'gemini-pro' })
+    .getGenerativeModel({ model: 'gemini-1.5-pro-latest' })
     .generateContentStream(buildGoogleGenAIPrompt(messages));
 
     console.log("Generated Gemini stream:", geminiStream);
